f93aa2fcedf3e5f1cc388aabe3528d246eafeb45
Eric <e.k.chi7@gmail.com>
2023-06-05 17:52:29 -0400

    updating poller for automobiles

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
+from common.json import ModelEncoder
+from .models import AutomobileVO, Technician, Appointment
+
+
+def api_list_technicians():
--- service/poll/poller.py
+++ service/poll/poller.py
@@ -9,6 +9,8 @@ sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service_project.settings")
 django.setup()
 
+
+from service_rest.models import AutomobileVO
 # Import models from service_rest, here. Ignore vs-code error hinting
 # from service_rest.models import Something
 
@@ -18,9 +20,13 @@ def poll(repeat=True):
         print('Service poller polling for data')
         try:
             # Write your polling logic, here
-            # Do not copy entire file
-            pass
-        
+            response = requests.get("http://inventory-api:8000/api/automobiles/")
+            content = json.loads(response.content)
+            for automobile in content["automobiles"]:
+                AutomobileVO.objects.update_or_create(
+                    vin=automobile['vin'],
+                    defaults={"sold": automobile["sold"]},
+                )
         except Exception as e:
             print(e, file=sys.stderr)
 

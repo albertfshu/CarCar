c41c0838da4156ae9efddb8199c61b40e49102a0
Eric <e.k.chi7@gmail.com>
2023-06-06 20:18:38 -0400

    added search bar but no functionality

--- ghi/app/src/ServiceHistory.js
+++ ghi/app/src/ServiceHistory.js
@@ -21,6 +21,13 @@ function ServiceHistory() {
         return undefined
     }
     return (
+        <form action="/" method="get">
+            <h1>Service History</h1>
+            <label htmlFor="header-search">
+                <span className="visually-hidden"></span>
+            </label>
+            <input type="text" id="header-search" placeholder="Search by VIN..." name="search"/>
+            <button type="submit">Search</button>
         <table className="table table-striped">
             <thead>
                 <tr>
@@ -36,19 +43,20 @@ function ServiceHistory() {
             <tbody>
             {appointments.map(appointment => {
                 return (
-                <tr key={appointment.id}>
-                    <td>{ appointment.name }</td>
+                    <tr key={appointment.id}>
                     <td>{ appointment.vin }</td>
                     <td>{ appointment.customer }</td>
-                    <td>{ appointment.date_time }</td>
-                    <td>{ appointment.date_time }</td>
+                    <td>{ new Date(appointment.date_time).toLocaleDateString() }</td>
+                    <td>{ new Date(appointment.date_time).toLocaleTimeString() }</td>
                     <td>{ appointment.technician }</td>
+                    <td>{ appointment.reason }</td>
                     <td>{ appointment.status }</td>
                 </tr>
                 );
             })}
             </tbody>
         </table>
+        </form>
       );
 }
 

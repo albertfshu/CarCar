3346c505adce9f9336e4a3970904424fe7a27979
Eric <e.k.chi7@gmail.com>
2023-06-06 20:09:04 -0400

    formatted dates on appointment list

--- ghi/app/src/AppointmentList.js
+++ ghi/app/src/AppointmentList.js
@@ -10,13 +10,10 @@ function AppointmentList() {
         if (response.ok) {
             const data = await response.json();
             setAppointments(data.appointments);
-            for (let appointment in data.appointments){
-                console.log(appointment.date_time)
 
-            }
+
         }
     }
-
 
     useEffect(() => {
         fetchAppointments();
@@ -72,8 +69,8 @@ function AppointmentList() {
                 <tr key={appointment.id}>
                     <td>{ appointment.vin }</td>
                     <td>{ appointment.customer }</td>
-                    <td>{ appointment.date_time }</td>
-                    <td>{ appointment.date_time }</td>
+                    <td>{ new Date(appointment.date_time).toLocaleDateString() }</td>
+                    <td>{ new Date(appointment.date_time).toLocaleTimeString() }</td>
                     <td>{ appointment.technician }</td>
                     <td>{ appointment.reason }</td>
                     <td>

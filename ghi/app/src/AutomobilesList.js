5083e637847aae357f57a8d1c0426c748c2d824f
Eric <e.k.chi7@gmail.com>
2023-06-08 13:29:25 -0400

    updated automobile list displaying automobile data

--- ghi/app/src/AutomobilesList.js
+++ ghi/app/src/AutomobilesList.js
@@ -2,6 +2,21 @@ import React, { useState, useEffect } from 'react';
 
 
 function AutomobilesList() {
+    const [automobiles, setAutomobiles] = useState([])
+
+    const fetchAutomobiles = async () => {
+        const response = await fetch('http://localhost:8100/api/automobiles/');
+
+        if (response.ok) {
+            const data = await response.json();
+            setAutomobiles(data.autos);
+        }
+    }
+
+    useEffect(() => {
+        fetchAutomobiles();
+    }, [])
+
     return(
         <table className="table table-striped">
         <thead>
@@ -15,13 +30,21 @@ function AutomobilesList() {
             </tr>
         </thead>
         <tbody>
-
-            <tr>
+        {automobiles.map(automobile => {
+            return (
+            <tr key={automobile.id}>
+                <td>{ automobile.vin }</td>
+                <td>{ automobile.color }</td>
+                <td>{ automobile.year }</td>
+                <td>{ automobile.model.name }</td>
+                <td>{ automobile.model.manufacturer.name }</td>
+                <td>{ automobile.sold ? "yes" : "no"}</td>
             </tr>
-
+            );
+        })}
         </tbody>
     </table>
-    )
+    );
 }
 
 export default AutomobilesList;

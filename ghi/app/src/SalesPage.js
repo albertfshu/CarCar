a7de756e41154e525beaad2964cf99bfc0e73c5c
Albert Shu <albert94shu@gmail.com>
2023-06-07 19:09:15 -0500

    List all sales

--- ghi/app/src/SalesPage.js
+++ ghi/app/src/SalesPage.js
@@ -24,36 +24,33 @@ function SalesPage() {
   }, []);
 
   return (
-    <div>
+    <>
+      <div>
       <h1>Sales</h1>
-      <table>
+      <table className="table table-striped">
         <thead>
           <tr>
             <th>Salesperson Employee ID</th>
             <th>Salesperson Name</th>
-            <th>Customer Name</th>
-            <th>Automobile VIN</th>
+            <th>Customer</th>
+            <th>VIN</th>
             <th>Price</th>
           </tr>
         </thead>
         <tbody>
-          {sales.map((sale) => {
-            console.log(sale)
-            console.log(sale.salesperson)
-            console.log(sale.customer)
-            return(
+          {sales.map((sale) => (
             <tr key={sale.id}>
-              <td>{sale.salesperson_id}</td>
-              <td>{sale.salesperson_name}</td>
-              <td>{sale.customer_id}</td>
-              <td>{sale.automobile_id}</td>
+              <td>{sale.salesperson.employee_id}</td>
+              <td>{`${sale.salesperson.first_name} ${sale.salesperson.last_name}`}</td>
+              <td>{`${sale.customer.first_name} ${sale.customer.last_name}`}</td>
+              <td>{sale.automobile.vin}</td>
               <td>${sale.price}</td>
             </tr>
-            )
-})}
+          ))}
         </tbody>
       </table>
     </div>
+    </>
   );
 }
 

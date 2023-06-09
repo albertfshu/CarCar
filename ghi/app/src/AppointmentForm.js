7de648d61ad87a660a810724798f520ac2b5d373
Eric <e.k.chi7@gmail.com>
2023-06-09 12:41:23 -0400

    fishing appointment form to get rid of value in data.status_id

--- ghi/app/src/AppointmentForm.js
+++ ghi/app/src/AppointmentForm.js
@@ -49,8 +49,7 @@ function AppointmentForm(){
         data.date_time = `${date} ${time}`;
         data.technician = technician;
         data.reason = reason;
-        data.status_id=1;
-        console.log(data.status)
+        console.log(data)
 
         const appointmentUrl = 'http://localhost:8080/api/appointments/';
         const fetchConfig = {
